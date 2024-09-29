a56782b2303b22941fabb8ed9029d3edc1f52c41
hellochenk <657301050@qq.com>
2024-09-29 14:45:16 +0800

    feat: update export type & add wrangler

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export'
+  // output: 'export'
+  output: 'standalone'
 };
+
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
+}
 
 export default nextConfig;
